4dfa356daec02df82a5033492d697f0e23c0ef13
Rahul7549 <rksharma15700@gmail.com>
2023-09-24 08:26:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+   const [newsList,setNewsList]=useState([]);
+   const fetchData=async ()=>{
+    let url="https://newsapi.org/v2/everything?q=tesla&from=2023-08-24&sortBy=publishedAt&apiKey=2e7cc35850954282a2c9761092639d1e"
+      let data=await fetch(url);
+      let parseData=await data.json();
+      setNewsList(parseData);
+      console.log("data",newsList);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={fetchData} >fetchData from service</button>
     </div>
   );
 }
